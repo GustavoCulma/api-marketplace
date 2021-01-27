a64fe8425807eea6727c5b4fcd026a3a71e77c3b
GustavoCulma <gaculma@misena.edu.co>
2021-01-27 14:36:35 -0500

    conteo y error 404

--- controllers/get.controller.php
+++ controllers/get.controller.php
@@ -1,17 +1,24 @@
-<?php 
+<?php
 class GetContoller
 /*=============================================
 =       peticion GET sin filtro              =
-=============================================*/	
+=============================================*/
 {
 	public function getData($table)
 	{
 		$response = GetModel::getData($table);
-		$json=array(
+		if (!empty($response )) {
+			$json=array(
 			'status' =>200,
+			'total'=>count($response),
 			'result' =>$response
 		);
-
+		} else {
+			$json=array(
+			'status' =>404,
+			'result' =>"Not found"
+		);
+		}
 		echo json_encode($json,http_response_code("$json[status]"));
 		return;
 	}
