5a9135c73c4e5f3c92f6bca5cf5528f4a55a42df
GustavoCulma <gaculma@misena.edu.co>
2021-01-26 20:54:11 -0500

    sin peticiones a la API

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
-<?php 
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('content-type: application/json; charset=utf-8');
 
 require_once"controllers/route.controller.php";
-
 $index = new RouteController();
 $index->index();
-return;
 
 
  ?>
--- routes/route.php
+++ routes/route.php
@@ -1,11 +1,29 @@
-<?php 
-$json=array(
-	'status' =>404,
-	'result' =>"Not found"  
-);
+<?php
+$routesArray = explode('/', $_SERVER["REQUEST_URI"]);
+$routesArray =array_filter($routesArray);
+/*=============================================
+= Cuando no se le hace ninguna peticion a la API=
+=============================================*/
+if (count($routesArray)==0) {
+	$json=array(
+		'status' =>404,
+		'result' =>"Not found"
+	);
 
-echo json_encode($json,http_response_code("$json[status]"));
+	echo json_encode($json,http_response_code("$json[status]"));
 
-return;
 
+	return;
+} else {
+
+	$json=array(
+		'status' =>200,
+		'result' =>"succes"
+	);
+
+	echo json_encode($json,http_response_code("$json[status]"));
+
+
+	return;
+}
 ?>
